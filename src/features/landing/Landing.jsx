9d1e073fe6b39a92e5c606c5aaefc1e6ecccb7f7
hussain-saeed <hussain.saeed.alsaadaney@gmail.com>
2025-11-09 02:56:32 +0200

    hero section

--- src/features/landing/Landing.jsx
+++ src/features/landing/Landing.jsx
@@ -1,9 +1,11 @@
 import Header from "../../components/Header";
+import Hero from "./sections/Hero";
 
 function Landing() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
---
+++ src/features/landing/sections/Hero.jsx
@@ -0,0 +1,48 @@
+import Container from "../../../components/Container";
+
+function Hero() {
+  return (
+    <div className="bg-(--secondary-section-color) min-h-[95vh] flex">
+      <Container className="flex flex-col items-center">
+        <h1
+          className="text-[56px] sm:text-[96px]"
+          style={{
+            marginTop: "72px",
+            marginBottom: "14px",
+            background: "var(--gradient-color)",
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+            fontWeight: "900",
+            textAlign: "center",
+          }}
+        >
+          DESIGN EASILY
+        </h1>
+        <p
+          className="w-[85%] md:w-[60%]"
+          style={{
+            marginBottom: "62px",
+            color: "rgba(0, 0, 0, 1)",
+            fontSize: "20px",
+            textAlign: "center",
+          }}
+        >
+          Enhance, retouch, remove backgrounds, and create stunning visuals in
+          seconds — no design skills needed.
+        </p>
+        <div
+          style={{ boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)" }}
+          className="w-[85%] md:w-[60%] h-auto overflow-hidden rounded-[30px] mb-[123px]"
+        >
+          <img
+            src="/images/hero.gif"
+            alt="Hero"
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default Hero;

cf02065b4e1b882b3632aca15afd3f344de4c4a6
hussain-saeed <hussain.saeed.alsaadaney@gmail.com>
2026-02-04 00:29:16 +0200

    page not found

--- src/admin/pages/AdminNotFound.jsx
+++ src/admin/pages/AdminNotFound.jsx
@@ -1,5 +1,34 @@
-export default function AdminNotFound() {
-  console.log("[PAGE] AdminNotFound");
+import Container from "../../components/Container";
+import { Link } from "react-router-dom";
+import { useMemo } from "react";
 
-  return <h2>404 - Admin Page Not Found</h2>;
+function AdminNotFound() {
+  const images = [
+    "/images/not-found-admin.png",
+    "/images/not-found-admin-2.png",
+  ];
+
+  // اختيار صورة عشوائية مرة واحدة عند الرندر
+  const randomImage = useMemo(() => {
+    const index = Math.floor(Math.random() * images.length);
+    return images[index];
+  }, []);
+
+  return (
+    <div>
+      <Container className="pt-30 pb-40 flex flex-col justify-center items-center">
+        <img src={randomImage} alt="not found" className="mb-5" />
+
+        <Link
+          to="/admin8yut91b9e22a/main"
+          className="text-[20px] sm:text-[24px] underline"
+          style={{ fontWeight: "900" }}
+        >
+          Back to Home
+        </Link>
+      </Container>
+    </div>
+  );
 }
+
+export default AdminNotFound;
